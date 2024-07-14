58e2201808ca68e999a150677eb60ae39eb3f5c8
noob <LinYongYangAg@gmail.com>
2024-07-15 00:47:06 +0800

    Date: 2024/7/15

---
+++ UartApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Uart_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uart u = new Uart("COM4", 9600);
+            u.OpenSerial();
+
+            for (int i = 0; i < 10; i++)
+            {
+                string line = u.ReadLines();
+                if (line != "")
+                {
+                    Console.Write(line);
+                }
+                Thread.Sleep(1000);
+            }
+
+            u.CloseSerial();
+        }
+    }
+}
