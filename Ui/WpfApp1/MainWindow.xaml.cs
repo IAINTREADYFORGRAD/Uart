9bc218a6fc0060c90088c5fcd15536d653ff1918
noob <LinYongYangAg@gmail.com>
2024-08-04 00:45:29 +0800

    Date: 2024/8/4

---
+++ Ui/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO.Ports;
+using System.Linq;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PopulateComboBoxWithPorts();
+        }
+
+        private void PopulateComboBoxWithPorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            string selectedPort1 = null;
+
+            if (ports.Length > 0)
+            {
+                comboBoxPorts1.ItemsSource = ports;
+                comboBoxPorts1.SelectedIndex = 0;
+
+                selectedPort1 = ports[0];
+            }
+
+            if (ports.Length > 1)
+            {
+                //
+                // port: parameter used within the lambda expression trepresenting 'ports'
+                //
+                string[] filteredPorts = ports.Where(port => port != selectedPort1).ToArray();
+                comboBoxPorts2.ItemsSource = filteredPorts;
+                comboBoxPorts2.SelectedIndex = 0;
+            }
+        }
+
+        private void comboBoxPorts_SelectionChanged1(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count == 0)
+            {
+                return;
+
+            }
+             
+            //
+            // cast the selected item from the ComboBox to a string
+            //
+            string selectedPort1 = e.AddedItems[0] as string;
+
+            string[] filteredPorts = SerialPort.GetPortNames().Where(port => port != selectedPort1).ToArray();
+            comboBoxPorts2.ItemsSource = filteredPorts;
+        }
+
+        private void comboBoxPorts_SelectionChanged2(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count == 0)
+            {
+                return;
+            }
+            
+            string selectedPort2 = e.AddedItems[0] as string;
+
+            string[] filteredPorts = SerialPort.GetPortNames().Where(port => port != selectedPort2).ToArray();
+            comboBoxPorts1.ItemsSource = filteredPorts;
+        }
+    }
+}
