c4b66e4c3cf4db5bd04e4415bbb0923eaad5716a
noob <LinYongYangAg@gmail.com>
2024-08-10 01:47:14 +0800

    Date: 2024/8/10

--- Ui/WpfApp1/obj/Release/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ Ui/WpfApp1/obj/Release/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c741c6b551e2ca360f455f035110636bc19e30af")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
