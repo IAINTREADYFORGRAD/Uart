97378507500dcafb9af8506e7dae405004415c82
noob <LinYongYangAg@gmail.com>
2024-08-18 00:52:14 +0800

    Date: 2024/8/18

--- Ui/WpfApp1/obj/Debug/net8.0-windows/WpfApp1_w4a2hjol_wpftmp.AssemblyInfo.cs
+++ Ui/WpfApp1/obj/Debug/net8.0-windows/WpfApp1_w4a2hjol_wpftmp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b759e0d15c644c558e47794642ce8aec5857b7a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
